1b1055b3ae7a67e0041ee6d05f0245df5359ddd8
oyekamal <autocratickamal1@gmail.com>
2022-12-12 11:22:19 +0500

    complete project

---
+++ flask_app/config.py
@@ -0,0 +1,15 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    #  use this setting when you have postgresql install 
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
+    #     or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
